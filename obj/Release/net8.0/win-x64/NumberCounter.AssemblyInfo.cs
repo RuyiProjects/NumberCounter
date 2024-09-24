72c264f88682794fd5be1b4abf2d53a5e41de7ad
Dakota Suffern <dakotasuffern@gmail.com>
2024-09-24 00:54:21 -0400

    New releases added built version to it!

--- obj/Release/net8.0/win-x64/NumberCounter.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/NumberCounter.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberCounter")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff2aa87b69b14b31e688c00de836984d78a8c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberCounter")]
